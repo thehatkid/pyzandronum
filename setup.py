8634f6a0509c5ff200b1f1a94f535aa93caa01bb
Sasha Ermohin <saha22023@gmail.com>
2022-01-27 10:49:41 +0700

    python module setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+readme = ''
+with open('README.md', encoding='utf-8') as fp:
+    readme = fp.read()
+
+setup(
+    name='pyzandronum',
+    version='0.1.0',
+    license='MIT',
+    description='Simple Python module for Zandronum server talking.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='hat_kid',
+    url='https://github.com/thehatkid/pyzandronum',
+    packages=['pyzandronum'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed'
+    ]
+)
