26d768e6e0ad9986bd8bdc0f99d09393573859de
Sasha Ermohin <saha22023@gmail.com>
2022-01-31 03:08:32 +0700

    update version

--- pyzandronum/__init__.py
+++ pyzandronum/__init__.py
@@ -6,7 +6,7 @@ __title__ = 'pyzandronum'
 __author__ = 'hat_kid'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2022-present hat_kid'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .zandronum import *
 from .asynchronous import AsyncServer
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as fp:
 
 setup(
     name='pyzandronum',
-    version='0.1.0',
+    version='0.2.0',
     license='MIT',
     description='Simple Python module for Zandronum server talking.',
     long_description=readme,
