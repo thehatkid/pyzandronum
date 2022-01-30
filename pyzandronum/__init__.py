9c25ae7d2319dc83a167359c3584e582b11b9285
Sasha Ermohin <saha22023@gmail.com>
2022-01-31 02:05:51 +0700

    Update __init__.py

--- pyzandronum/__init__.py
+++ pyzandronum/__init__.py
@@ -4,11 +4,9 @@ Zandronum server querying wrapper.
 
 __title__ = 'pyzandronum'
 __author__ = 'hat_kid'
-__license__ = ''
+__license__ = 'MIT'
 __copyright__ = 'Copyright 2022-present hat_kid'
 __version__ = '0.1.0'
 
-from typing import NamedTuple
-from typing import Literal
-
 from .zandronum import *
+from .asynchronous import AsyncServer
